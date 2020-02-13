4bb2d7d183d970c892ba9e8c020dc1230dde2609
mbborromeo <mbborromeo@gmail.com>
2020-02-12 16:11:59 -0800

    Javascript working functionality

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # instanews-nytimes
 Instanews - NY Times API call
+
+# To install
+npm install
+
+# To run
+gulp
--- js/script.js
+++ js/script.js
@@ -1,11 +1,83 @@
-$(function() {
-  $('body').addClass('js');
-  var $menu = $('#menu'),
-      $menulink = $('.menu-link');
+document.addEventListener("DOMContentLoaded", function(){
+//$(function() {
+  // Variables
+  const LIMIT = 12;
+  const MY_API_KEY='BvCBQeH2bfwoKZ8Uabwe0ynMvv1yx8VZ';
+
+  // References
+  const stories = document.getElementById('stories');  
+  const dropdown = document.getElementById('category');
+
+  // Loading graphic
+  const loader = document.createElement('img');
+  loader.setAttribute('id', 'loader');
+  loader.setAttribute('src', 'images/ajax-loader.gif');
+  loader.setAttribute('style', 'display:none');
+  stories.append(loader);
+
+  // Functions
+  function generateThumbs(number, array){
+    // create new list
+    const list = document.createElement('ul');
+
+    for( let i=0; i < number; i++ ){
+      // TO DO: only display articles that have an image, use filter...
+      if(array[i].multimedia[0].url){
+        const item = document.createElement('li');
+        const link = document.createElement('a');
+        link.setAttribute('target', '_blank');
+        link.setAttribute('href', array[i].url);
+        const thumb = document.createElement('div');
+        thumb.setAttribute('class', 'thumb');
+        thumb.setAttribute('style', `background-image:url( ${ array[i].multimedia[0].url } )`);
+        link.append(thumb);
+        item.append(link);
+        list.append(item);
+      }      
+    }
+
+    stories.append(list);
+  }
+
   
-  $menulink.click(function() {
-    $menulink.toggleClass('active');
-    $menu.toggleClass('active');
-    return false;
+  // Event Handlers
+  dropdown.addEventListener('change', function(event){    
+    // get value that it changed to
+    // $selectedCategory = $(this).children('option:selected').val(); // .text() // $(this).val()
+    const selectedCategory = this.value; // event.srcElement.value
+
+    // show loading graphic
+    loader.setAttribute('style', 'display:block');
+
+    // Hide previous list before deleting it
+    if( document.querySelector('ul') ) {
+      let existingList = document.querySelector('ul');
+      existingList.setAttribute('display', 'none');
+      existingList.parentNode.removeChild(existingList);
+    }
+    
+    // using jQuery ajax method
+    $.ajax({
+      method: 'GET',
+      url: `https://api.nytimes.com/svc/topstories/v2/${ selectedCategory }.json?api-key=${ MY_API_KEY }` 
+      // better to put key in external file
+    })
+    .done( function(data){
+      console.log(data);
+
+      if( data.results.length > 0 ){      
+        // hide loading loop image
+        loader.setAttribute('style', 'display:none');
+        
+        if( data.results.length > LIMIT ){
+          generateThumbs( LIMIT, data.results );
+        } else {
+          generateThumbs( data.results.length, data.results );
+        }     
+      }      
+    });
   });
-});
+  
+  
+// }); end document.ready
+}); // end Javascript DOMContentLoaded wrapper
